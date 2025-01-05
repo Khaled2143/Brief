063f68b9764964e6d009aa2690fcbd48052e3009
Khaled2143 <khahmed2143@gmail.com>
2025-01-04 19:28:11 -0500

    Users can now submit comments on discussion posts

--- component/TotalDiscussion.js
+++ component/TotalDiscussion.js
@@ -1,16 +1,54 @@
 import { StyleSheet, View, Text, FlatList, Pressable } from "react-native";
 import axios from "axios";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import { useNavigation } from "@react-navigation/native";
+import { TextInput } from "react-native-gesture-handler";
+import { UserContext, UserProvider } from "../context/UserContext";
 
 const TotalDiscussion = ({ route }) => {
   const { discussion } = route.params;
+  const [content, setContent] = useState("");
+  const { _id: id } = discussion;
+  const { user, setUser } = useContext(UserContext);
+  console.log("CURRENT USER:", user.username);
+  console.log("HERES THE CONTENT", content);
+
+  const handleSubmit = () => {
+    if (!content) {
+      alert("Cannot submit without content being filled");
+      return;
+    }
+    axios
+      .post(`http://localhost:5001/api/discussions/${id}/comments`, {
+        content,
+        username: user.username,
+      })
+      .then((resposne) => {
+        console.log("Comment Posted", resposne.data);
+      })
+      .catch((error) => {
+        console.error(
+          "Error posting comment:",
+          error.response?.data?.message || error.message
+        );
+      });
+  };
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>{discussion.title}</Text>
 
       <Text style={styles.content}>{discussion.content}</Text>
+
+      <TextInput
+        style={styles.textInput}
+        onChangeText={setContent}
+        value={content}
+        placeholder="Content"
+      />
+      <Pressable style={styles.button} onPress={handleSubmit}>
+        <Text>Send</Text>
+      </Pressable>
     </View>
   );
 };
@@ -27,6 +65,17 @@ const styles = StyleSheet.create({
   content: {
     fontSize: 20,
     textAlign: "center",
+  },
+  textInput: {
+    borderWidth: 2,
+    marginTop: 10,
+    width: "90%",
+  },
+  button: {
+    backgroundColor: "#6200ee",
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 10,
   },
 });
 
